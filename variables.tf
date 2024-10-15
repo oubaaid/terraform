d767e6a4a57d165eeb7b3d3e25e778dabc2bcc49
oubaaid <98980894+oubaaid@users.noreply.github.com>
2024-10-15 04:01:13 +0200

    full copy

--- variables.tf
+++ variables.tf
@@ -6,38 +6,36 @@ variable "instance_type" {
 variable "ami_filter" {
   description = "Name filter and owner for AMI"
 
-  type    = object({
+  type    = object ({
     name  = string
     owner = string
   })
 
   default = {
-  name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
-  owner = "979382823631"
+    name  = "bitnami-tomcat-*-x86_64-hvm-ebs-nami"
+    owner = "979382823631" # Bitnami
   }
 }
 
 variable "environment" {
-  description = "Development Environment"
+  description = "Deployment environment"
 
-  type = object ({
+  type        = object ({
     name           = string
     network_prefix = string
   })
-
   default = {
-  name            = "dev"
-  network_prefix = "10.0"
+    name           = "dev"
+    network_prefix = "10.0"
   }
 }
 
-
-variable "asg_min_size" {
-  description = "Minimum number of instances in the ASG"
-  default           = 1
+variable "asg_min" {
+  description = "Minimum instance count for the ASG"
+  default     = 1
 }
 
-variable "asg_max_size" {
-  description = "Maximum number of instances in the ASG"
-  default           = 2
+variable "asg_max" {
+  description = "Maximum instance count for the ASG"
+  default     = 2
 }
